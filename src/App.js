dd57bd80c03f535cdf4c00106c603db9b103cbd7
saloum45 <saloumfall45@gmail.com>
2023-12-18 12:35:45 +0000

    first one

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mine">
+      <p>Explication de la structure du projet React</p>
+    </div>
+  );
+}
+
+export default App;
