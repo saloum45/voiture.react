917d98eaa7fdd14f5235a4914027ac7c93b6c5ee
unknown <thiernohabibbah176@gmail.com>
2023-12-18 18:25:28 +0100

    push

--- src/Mesvoitures.js
+++ src/Mesvoitures.js
@@ -1,25 +1,30 @@
 // MesVoitures.js
 import React, { Component, useState } from "react";
 import Voiture from "./Voiture"; // Import du composant Voiture
-import Header from "../Header";
 
 class MesVoitures extends Component {
   constructor(props) {
     super(props);
     this.state = {
       voitures: [
-        { id: 1, image: "", marque: "Toyota", couleur: "Rouge", annee: 2020 },
-        { id: 2, image: "", marque: "Honda", couleur: "Bleu", annee: 2019 },
-        { id: 3, image: "", marque: "Ford", couleur: "Vert", annee: 2018 },
-        { id: 4, image: "", marque: "BMW", couleur: "violet", annee: 2023 },
+        { id: 1, image: "", marque: "Toyota", couleur: "Rouge", annee: 1986 },
+        { id: 2, image: "", marque: "Honda", couleur: "Bleu", annee: 2002 },
+        { id: 3, image: "", marque: "Ford", couleur: "Vert", annee: 2014 },
+        { id: 4, image: "", marque: "BMW", couleur: "violet", annee: 2020 },
       ],
     };
   }
-  // const {nom , setnom}= useState("bah")
+
+  getAge = (anneeFabrication) => {
+    const ActuelleDate = new Date().getFullYear();
+    const Age = ActuelleDate - anneeFabrication;
+    return Age;
+  };
 
   render() {
     return (
       <div className="Mesvoitures w-75 m-auto">
+        <h1 id="contentVoiture">Liste des Voitures</h1>
         <table class="table mt-5">
           <thead>
             <tr>
@@ -27,11 +32,13 @@ class MesVoitures extends Component {
               <th scope="col">Marque</th>
               <th scope="col">Couleur</th>
               <th scope="col">Année</th>
+              <th scope="col">Age</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
             {this.state.voitures.map((voiture, index) => (
-              <Voiture voiture={voiture} />
+              <Voiture voiture={voiture} age={this.getAge(voiture.annee)} />
             ))}
           </tbody>
         </table>
--- src/Voiture.js
+++ src/Voiture.js
@@ -1,12 +1,14 @@
-
-const Voiture = ({ voiture }) => {
+const Voiture = ({ voiture, age }) => {
   return (
     <tr>
       <td>{voiture.id}</td>
       <td>{voiture.marque}</td>
       <td>{voiture.couleur}</td>
       <td>{voiture.annee}</td>
-      
+      <td>{age} ans</td>
+      <td>
+        <button className="btn btn-primary">Afficher/Cacher</button>
+      </td>
     </tr>
   );
 };
