2d4adbe264189b807b4818b0b492e6d555276594
unknown <thiernohabibbah176@gmail.com>
2023-12-19 10:13:28 +0100

    push

--- src/Voiture.js
+++ src/Voiture.js
@@ -1,13 +1,23 @@
+import React, { useState } from "react";
+
 const Voiture = ({ voiture, age }) => {
+  const [showAge, setShowAge] = useState(false);
+
+  const afficherage = () => {
+    setShowAge(!showAge);
+  };
+
   return (
     <tr>
       <td>{voiture.id}</td>
       <td>{voiture.marque}</td>
       <td>{voiture.couleur}</td>
       <td>{voiture.annee}</td>
-      <td>{age} ans</td>
+      <td>{showAge ? age + " ans" : null}</td>
       <td>
-        <button className="btn btn-primary">Afficher/Cacher</button>
+        <button className="btn btn-primary" onClick={afficherage}>
+          {showAge ? "Cacher" : "Afficher"}
+        </button>
       </td>
     </tr>
   );
